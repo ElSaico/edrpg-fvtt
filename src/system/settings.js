b50512418fc10f7fb0c4da58cbf1ce5973aec6e2
Bruno Marques <bruno.oliveira_marques@siemens.com>
2024-03-05 22:49:00 -0300

    Using TRL's settings registry

--- src/system/settings.js
+++ src/system/settings.js
@@ -1,37 +1,59 @@
+import { TJSGameSettings } from "#runtime/svelte/store/fvtt/settings";
+
+export const settings = new TJSGameSettings("edrpg");
+
 /**
  *
  */
 export default function registerSystemSettings() {
-   game.settings.register("edrpg", "systemMaxBackgrounds", {
-      name: "SETTINGS.systemMaxBackgrounds.name",
-      hint: "SETTINGS.systemMaxBackgrounds.hint",
-      scope: "world",
-      config: true,
-      default: 4,
-      type: Number,
-   });
-   game.settings.register("edrpg", "defaultDifficultyNumber", {
-      name: "SETTINGS.defaultDifficultyNumber.name",
-      hint: "SETTINGS.defaultDifficultyNumber.hint",
-      scope: "world",
-      config: true,
-      default: 9,
-      type: Number,
-   });
-   game.settings.register("edrpg", "allowToRerollAllTests", {
-      name: "SETTINGS.allowToRerollAllTests.name",
-      hint: "SETTINGS.allowToRerollAllTests.hint",
-      scope: "world",
-      config: true,
-      default: false,
-      type: Boolean,
-   });
-   game.settings.register("edrpg", "socialFactorCap", {
-      name: "SETTINGS.socialFactorCap.name",
-      hint: "SETTINGS.socialFactorCap.hint",
-      scope: "world",
-      config: true,
-      default: 4,
-      type: Number,
-   });
-} 
+   settings.registerAll([
+      {
+         namespace: "edrpg",
+         key: "systemMaxBackgrounds",
+         options: {
+            name: "SETTINGS.systemMaxBackgrounds.name",
+            hint: "SETTINGS.systemMaxBackgrounds.hint",
+            scope: "world",
+            config: true,
+            default: 4,
+            type: Number,
+         },
+      },
+      {
+         namespace: "edrpg",
+         key: "defaultDifficultyNumber",
+         options: {
+            name: "SETTINGS.defaultDifficultyNumber.name",
+            hint: "SETTINGS.defaultDifficultyNumber.hint",
+            scope: "world",
+            config: true,
+            default: 9,
+            type: Number,
+         },
+      },
+      {
+         namespace: "edrpg",
+         key: "allowToRerollAllTests",
+         options: {
+            name: "SETTINGS.allowToRerollAllTests.name",
+            hint: "SETTINGS.allowToRerollAllTests.hint",
+            scope: "world",
+            config: true,
+            default: false,
+            type: Boolean,
+         },
+      },
+      {
+         namespace: "edrpg",
+         key: "socialFactorCap",
+         options: {
+            name: "SETTINGS.socialFactorCap.name",
+            hint: "SETTINGS.socialFactorCap.hint",
+            scope: "world",
+            config: true,
+            default: 4,
+            type: Number,
+         },
+      },
+   ]);
+}
